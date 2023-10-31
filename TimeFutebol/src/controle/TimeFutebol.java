10cd21976c65652be8fd049d00812d711218f01b
Matheus Zimmermann <125280019+matheuszimmermann@users.noreply.github.com>
2023-10-31 16:11:52 -0300

    Add files via upload

---
+++ TimeFutebol/src/controle/TimeFutebol.java
@@ -0,0 +1,71 @@
+package controle;
+
+import java.util.ArrayList;
+import modelo.Time;
+
+
+public class TimeFutebol {
+    
+    private ArrayList<Time> listaTimes = new ArrayList<>();
+    
+    public ArrayList<Time> getListarTimes() {
+        return listaTimes;
+    }
+
+    public void setListarTimes(ArrayList<Time> listaDeTimes) {
+        this.listaTimes = listaDeTimes;
+    }
+
+    
+    public void cadastrarTime() {
+        System.out.println("...:::  Adicionar Time :::...");
+        System.out.println("--------------------------------");
+
+        Time timess = new Time();
+        timess.lerTimes();
+        listaTimes.add(timess);
+
+        System.out.println("----------------------------");
+    }
+
+    public void listarTimesDisponiveis() {
+        System.out.println("...:::  Listar Times :::...");
+        System.out.println("----------------------------");
+
+        for (int i = 0; i < listaTimes.size(); i++) {
+            Time timess = listaTimes.get(i);
+            timess.listarTimes();
+        }
+
+        System.out.println("----------------------------");
+    }
+
+    public void visualizarTimesDisponiveis() {
+        System.out.println("...:::  Visualizar Times :::...");
+        System.out.println("----------------------------");
+
+        for (int i = 0; i < listaTimes.size(); i++) {
+            Time timess = listaTimes.get(i);
+            timess.visualizarTimes();
+        }
+
+        System.out.println("----------------------------");
+    }
+    
+     public void removerTimesDisponiveis() {
+        System.out.println("...:::  Remover Time :::...");
+        System.out.println("----------------------------");
+
+        for (int i = 0; i < listaTimes.size(); i++) {
+            Time timess = listaTimes.remove(listaTimes.size() - 1);
+            timess.visualizarTimes();
+        }
+
+        System.out.println("----------------------------");
+        System.out.println("Time Removido!!!");
+        System.out.println("----------------------------");
+
+    }
+    
+
+}
